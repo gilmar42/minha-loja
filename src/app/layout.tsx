408f8009381233e91a658c76f93a9c7f6a57d006
gilmar42 <100540954+gilmar42@users.noreply.github.com>
2023-10-28 20:41:45 -0300

    Adicionando navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Navbar from "./components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,8 +15,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt-br">
+      <body className={inter.className}>
+        <Navbar />
+        <main className="bg-slate-700 h-screen p-16">{children}</main>
+      </body>
     </html>
   );
 }
